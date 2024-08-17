b686ddac487f4a4e4a432da9a7ce7c9d702a08cb
Thang Nguyen <>
2024-08-17 11:11:50 +0700

    Update

--- SteamFeeds/Controller/FavoritedAppsViewController.swift
+++ SteamFeeds/Controller/FavoritedAppsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FavoritedFeedsViewController.swift
+//  FavoritedAppsViewController.swift
 //  SteamFeeds
 //
 //  Created by Thang Nguyen on 17/8/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FavoritedFeedsViewController: UIViewController {
+class FavoritedAppsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +23,5 @@ class FavoritedFeedsViewController: UIViewController {
             }
         }
     }
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
