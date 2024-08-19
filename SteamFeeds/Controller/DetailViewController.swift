b5be57f67604e4b08a7be1c773a1d42bfececd62
Thang Nguyen <>
2024-08-19 22:56:16 +0700

    Implement webkit

--- SteamFeeds/Controller/DetailViewController.swift
+++ SteamFeeds/Controller/DetailViewController.swift
@@ -6,26 +6,43 @@
 //
 
 import UIKit
+import WebKit
+import SafariServices
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var navigationBar: UINavigationItem!
     
     var news: News!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateWebViewContent()
+        navigationBar.title = news.title
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateWebViewContent() {
+        guard let content = news.content else { return }
+        
+        let htmlString = """
+<html>
+<body>
+<font size="7">
+\(content.replacingOccurrences(of: "\n", with: "<br>"))
+</font>
+</body>
+</html>
+"""
+        if let decodedString = decodeUnicodeEscapeSequences(htmlString) {
+            webView.loadHTMLString(decodedString, baseURL: nil)
+        }
     }
-    */
-
+    
+    @IBAction func openExternalURL(_ sender: Any) {
+        if let url = URL(string: news.url!) {
+            let safariVC = SFSafariViewController(url: url)
+            present(safariVC, animated: true, completion: nil)
+        }
+    }
 }
--- SteamFeeds/Controller/ExtensionViewController.swift
+++ SteamFeeds/Controller/ExtensionViewController.swift
@@ -29,4 +29,25 @@ extension UIViewController {
             }
         }
     }
+    
+    /// Decode unicode character of steam content
+    func decodeUnicodeEscapeSequences(_ string: String) -> String? {
+        let pattern = #"\\u([0-9A-Fa-f]{4})"#
+        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        
+        let nsString = string as NSString
+        let matches = regex?.matches(in: string, options: [], range: NSRange(location: 0, length: nsString.length))
+        
+        var decodedString = string
+        matches?.reversed().forEach { match in
+            if let range = Range(match.range(at: 1), in: string) {
+                let hexCode = String(string[range])
+                if let scalar = UnicodeScalar(Int(hexCode, radix: 16)!) {
+                    decodedString = (decodedString as NSString).replacingCharacters(in: match.range, with: String(scalar))
+                }
+            }
+        }
+        
+        return decodedString
+    }
 }
